d8761c6f0aaca907ea993f862f29ee2f55e9d857
ahernandez <hernandez.agustinik@gmail.com>
2023-11-24 21:19:47 -0300

    Minor changes in zonaprop tests

--- property_scraper/tests/test_zonapropScraper.py
+++ property_scraper/tests/test_zonapropScraper.py
@@ -6,8 +6,6 @@ from src.browser import Browser
 from src.scraper import Scraper
 from bs4 import BeautifulSoup
 
-from unittest.mock import MagicMock
-
     
 @pytest.fixture
 def fixture_data():
@@ -20,15 +18,18 @@ def fixture_data():
 def scraper():
     return Scraper(Browser())
 
+def getPricesFromFixtureData():
+    np.array([
+            ['Consultar precio'],['Consultar precio'], ['$ 200.000'],['Consultar precio'],['Consultar precio'],
+            ['Consultar precio'],['$ 350.000'],['$ 400.000'],['$ 380.000'],['Consultar precio'],
+            ['Consultar precio'],['Consultar precio'],['$ 370.000'],['$ 350.000'],['Consultar precio'],
+            ['Consultar precio'],['$ 230.000'],['Consultar precio'],['$ 400.000'],['$ 380.000']
+        ])
+
     
 def test_validatePropertyPrices(fixture_data, scraper):
     properties_prices_data = scraper.getPricesFromPropertiesData(fixture_data).values
     
-    expected_data = np.array([
-        ['Consultar precio'],['Consultar precio'], ['$ 200.000'],['Consultar precio'],['Consultar precio'],
-        ['Consultar precio'],['$ 350.000'],['$ 400.000'],['$ 380.000'],['Consultar precio'],
-        ['Consultar precio'],['Consultar precio'],['$ 370.000'],['$ 350.000'],['Consultar precio'],
-        ['Consultar precio'],['$ 230.000'],['Consultar precio'],['$ 400.000'],['$ 380.000']
-    ])
+    expected_data = getPricesFromFixtureData()
     
     assert np.array_equal(properties_prices_data, expected_data)
