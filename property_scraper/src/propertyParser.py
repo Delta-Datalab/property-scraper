40bb712df3f14922e02cc2df0db22e936c5b08a8
agushernandezz <ahernandez@dc.uba.ar>
2023-12-05 19:52:07 -0300

    Add format

--- property_scraper/src/propertyParser.py
+++ property_scraper/src/propertyParser.py
@@ -2,14 +2,15 @@ import numpy as np
 
 from src.property import ZonaPropProperty
 
-class PropertyParser: 
+
+class PropertyParser:
     def __init__(self):
         pass
-        
+
     def get_propertyType(self, domain):
         if domain == "www.zonaprop.com.ar":
             property_type = ZonaPropProperty()
         else:
             raise ValueError("Invalid type of Property")
 
-        return (property_type)
+        return property_type
