e9ee8221c2b74ed18915276b6e1d54c6967f82d8
Juan Alvarez Colombo <jialvarez@dc.uba.ar>
2023-12-05 17:38:41 -0300

    PropertyParser Class constructed

---
+++ property_scraper/src/propertyParser.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from src.property import ZonaPropProperty
+
+class PropertyParser: 
+    def __init__(self):
+        pass
+        
+    def get_propertyType(self, domain):
+
+        if domain == "www.zonaprop.com.ar":
+            property_type = ZonaPropProperty()
+        else:
+            raise ValueError("Invalid type of Property")
+
+        return (property_type)
