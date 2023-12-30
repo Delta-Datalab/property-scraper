ba9a098421011fbb3ede21e3b99c6ddf309b5015
solcalloni <solcicalloni@gmail.com>
2023-12-29 23:44:32 -0300

    Fixing bug

--- property_scraper/property_scraper.py
+++ property_scraper/property_scraper.py
@@ -1,19 +1,5 @@
 from src.extract import Extract
 import click
-import logging
-import os
-
-log_directory = os.path.dirname(LOG_DIR)
-if not os.path.exists(log_directory):
-    os.makedirs(log_directory)
-
-os.path.exists(LOG_DIR) or open(LOG_DIR, "w").close()
-
-logging.basicConfig(
-    filename=LOG_DIR,
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s",
-)
 
 
 @click.command()
--- property_scraper/src/scraper.py
+++ property_scraper/src/scraper.py
@@ -4,6 +4,19 @@ from datetime import datetime
 from config import *
 
 import logging
+import os
+
+log_directory = os.path.dirname(LOG_DIR)
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+os.path.exists(LOG_DIR) or open(LOG_DIR, "w").close()
+
+logging.basicConfig(
+    filename=LOG_DIR,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s",
+)
 
 
 class Scraper:
