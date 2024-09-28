242a4b0eb3b7b0aa54502328235107e56e543316
Juan Alvarez Colombo <93490410+JuaniAlvarezC@users.noreply.github.com>
2024-09-27 22:59:07 -0300

    Integration of a Flask API for Property Data Retrieval and Filtering (#83)
    * Add first files
    
    * Added API functionalities and first endpoints
    
    * Minor changes
    
    * Starting Flask API for Property Data Retrieval and Filtering
    
    * Updates on API
    
    * Files formatted

---
+++ api/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, request, g
+from utils import executeQuery, filterHandler, conn, handlers
+
+app = Flask(__name__)
+
+
+@app.route("/locations")
+def get_locations():
+    """
+    Retrieves distinct locations from the property_data table and returns them as a JSON response.
+    """
+    query = "SELECT DISTINCT location FROM property_data ORDER BY location ASC;"
+    locations = [row[0] for row in executeQuery(query)]
+    return jsonify({"locations": locations})
+
+
+@app.route("/properties/filter")
+def get_properties_by_filter():
+    """
+    Filters properties based on the query parameters provided in the request.
+    """
+    try:
+        filters = {key: value for key, value in request.args.items() if key in handlers}
+        where_clause = [filterHandler(key, value) for key, value in filters.items()]
+
+        query = f"SELECT * FROM property_data WHERE {' AND '.join(where_clause)};"
+        properties = executeQuery(query)
+
+        return jsonify(properties)
+
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        conn.rollback()
+        return jsonify({"error": str(e)}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
